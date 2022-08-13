b60877a8a9537213c8a7e230255b9b6f9a3a0a1b
Crowdbotics <team@crowdbotics.com>
2022-08-13 14:04:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lupus_fighters_of_a_36765",
+  url: "https://lupus_fighters_of_a_36765.botics.co",
+  api: "https://lupus_fighters_of_a_36765.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
